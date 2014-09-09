514929c672bf546b3e55b57adedca182888d6384
aj-patel <connecttoaj@gmail.com>
2014-09-09 14:04:32 +0530

    API for maxCharOccurrence Fixes #1

--- src/main/java/StringsUtils/StringsUtils.java
+++ src/main/java/StringsUtils/StringsUtils.java
@@ -1,14 +1,39 @@
 package StringsUtils;
 
 /**
- * Hello world!
+ * Utility for operations on common String manipulation.
+ * @author ajay.patel
  *
  */
 public class StringsUtils 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println("testing the commit");
-    }
+	
+	/***
+	 * getMaximumOcurringCharacter(final String inputString)
+	 * get the maximum occurring character in inputString
+	 * @param inputString
+	 * @return
+	 */
+	public static char getMaximumOccurringCharacter(final String inputString){
+	   
+	   if(inputString != null && inputString.length() !=0){
+			final char[] inputStringArray= inputString.toCharArray();
+			int countChar[] =new int[59]; 
+			int maxOccurance = 0;
+			int charToReturn=0;
+			int index = 0;
+			for (char i : inputStringArray) {
+				index = (int)i -64;
+				countChar[index]=(countChar[index])+1;
+				if(maxOccurance < countChar[index]){
+					maxOccurance=countChar[index];
+					charToReturn=index+64;
+				}
+			}
+			return (char)charToReturn;
+		}
+		else{
+			throw new NullPointerException();
+		}
+   }
 }
